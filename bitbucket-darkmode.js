ea7b0d398b1085c996b5dbc441973063603fc5e7
dude lao <brycelipscomb@gmail.com>
2022-02-08 20:54:06 -0500

    testing things

---
+++ bitbucket-darkmode.js
@@ -0,0 +1,7 @@
+document.body.style.backgroundColor = "#202124"
+document.body.style.color = "white"
+
+r = document.querySelector(":root");
+r.style.setProperty('--text-highEmphasis', 'white')
+r.style.color = "white";
+document.querySelectorAll("div").style.color = "white"
